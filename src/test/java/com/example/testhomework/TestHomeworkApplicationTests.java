fa29faa3a18e6ef6d0d1887877bd5ec954260e80
Lubava <Lwm28@rambler.ru>
2023-04-13 16:05:48 +0300

    первый коммит

---
+++ src/test/java/com/example/testhomework/TestHomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testhomework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestHomeworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
